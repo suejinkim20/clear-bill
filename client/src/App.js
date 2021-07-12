5a2c113ccae76acc63dd0bf31bc9837394912eb6
Suejin Kim <kim.suejin.20@gmail.com>
2021-07-12 18:48:14 -0400

    nav bar working

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+// Pages
 import Home from './pages/Home';
 import SignUp from './pages/SignUp';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+
+// Components
 import Nav from './components/Nav';
 
 
 function App() {
   return (
       <Router>
+        <Nav />
+
         <Switch>
           <Route exact path="/">
             <Home />
---
+++ client/src/components/Nav.js
@@ -0,0 +1,17 @@
+import { AppBar, Tabs, Tab } from '@material-ui/core'
+import { Link } from 'react-router-dom'
+
+export default function Nav() {
+    return (
+        <div>
+            <AppBar position="static">
+                <Tabs>
+                    <Tab label="Home" component={Link} to="/" />
+                    <Tab label="Login" component={Link} to="/login" />
+                    <Tab label="SignUp" component={Link} to="/signup"/>
+                    <Tab label="Dashboard" component={Link} to="/dashboard" />
+                </Tabs>
+            </AppBar>
+        </div>
+    )
+}
