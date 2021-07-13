936e2ea3246081338680f7e1aad18044fb02df31
Taylor Lilley <lilleyt09@gmail.com>
2021-07-12 20:54:08 -0400

    added email and password fields to login page along with login button

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Box, TextField, Button } from '@material-ui/core';
 
 export default function Login() {
     return (
-        <div>
-            Login Page
-        </div>
+        <Box component="span" label="Login" m={1}>                
+        <form noValidate autoComplete="off">
+            <TextField id="outlined-basic" label="Email" variant="outlined" />
+            <TextField id="outlined-basic" label="Password" variant="outlined" />
+        </form>
+        <Button variant="contained" color="primary">
+            Login
+        </Button>
+        </Box>
     )
 }
