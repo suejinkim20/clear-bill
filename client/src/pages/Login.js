9acee5812ca8a673142dd7ff22b897b3ae69a20f
Taylor Lilley <lilleyt09@gmail.com>
2021-07-12 21:25:46 -0400

    fixed formatting on login form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,16 +1,34 @@
 import React from 'react';
-import { Box, TextField, Button } from '@material-ui/core';
+import { Box, Button, Container, TextField } from '@material-ui/core';
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     '& > *': {
+//       margin: theme.spacing(1),
+//       width: '25ch',
+//     },
+//   },
+// }));
 
 export default function Login() {
-    return (
-        <Box component="span" label="Login" m={1}>                
-        <form noValidate autoComplete="off">
+//   const classes = useStyles();
+
+  return (
+    <div>
+    <br></br>
+    <br></br>
+    <Container maxWidth="sm" style={{border: '1px solid'}}><h1 style={{textAlign: "center"}} >Login</h1>
+    <Box component="span" label="Login" m={1}> 
+        <form noValidate autoComplete="off">   
             <TextField id="outlined-basic" label="Email" variant="outlined" />
             <TextField id="outlined-basic" label="Password" variant="outlined" />
         </form>
         <Button variant="contained" color="primary">
             Login
         </Button>
-        </Box>
+        <br></br>
+        <br></br>
+    </Box></Container>
+    </div>
     )
 }
