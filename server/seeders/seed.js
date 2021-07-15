ef3ca6c93f1a513b57f3794b75316bbcee251d52
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2021-07-14 21:07:02 -0400

    Added to billseeds.json file

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,2 +1,9 @@
 const db = require('./connection');
 const { User, Bill } = require('../models');
+const billSeeds = require ("./billSeeds.json");
+const userSeeds = require ("./userSeeds.json");
+
+// db.once("open", async () => {
+//     await 
+
+// });
