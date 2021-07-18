35abb4438cc2c4d941d36270e19ebdfdbcd013c7
Suejin Kim <kim.suejin.20@gmail.com>
2021-07-17 23:12:33 -0400

    seeders working

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,9 +1,21 @@
-const db = require('./connection');
+const db = require('../config/connection');
 const { User, Bill } = require('../models');
 const billSeeds = require ("./billSeeds.json");
 const userSeeds = require ("./userSeeds.json");
 
-// db.once("open", async () => {
-//     await 
+db.once("open", async () => {
+    await User.deleteMany({});
+    await Bill.deleteMany({});
 
-// });
+    const users = await User.insertMany(userSeeds);
+    const bills = await Bill.insertMany(billSeeds);
+
+    for (newBill of bills) {
+        const tempUser = users[Math.floor(Math.random() * users.length)];
+        tempUser.bills.push(newBill._id);
+        await tempUser.save();
+    }
+    console.log('All Done!');
+
+process.exit(0);
+});
