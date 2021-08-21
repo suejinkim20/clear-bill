a141575fb0dffde81222eae202d6b3e07eb85037
Suejin Kim <kim.suejin.20@gmail.com>
2021-08-21 10:17:37 -0400

    removed dashboard tab from top nav so it can only be accessed by logging in

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -9,7 +9,7 @@ export default function Nav() {
                     <Tab label="Home" component={Link} to="/" />
                     <Tab label="Login" component={Link} to="/login" />
                     <Tab label="SignUp" component={Link} to="/signup"/>
-                    <Tab label="Dashboard" component={Link} to="/dashboard" />
+                    {/* <Tab label="Dashboard" component={Link} to="/dashboard" /> */}
                 </Tabs>
             </AppBar>
         </div>
